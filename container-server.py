37d1456c9cc81c96ce6c3cf0cee1e826bae41024
Majed <xz.3@hotmail.com>
2015-11-04 16:40:53 +0000

    new

--- container-server.py
+++ container-server.py
@@ -35,8 +35,7 @@ DELETE /images                      Delete all images
 @app.route('/containers', methods=['GET'])
 def containers_index():
 
-   curl -s -X GET -H 'Accept: application/json'
-  http://localhost:8080/containers | python -mjson.tool
+   curl -s -X GET -H 'Accept: application/json' http://localhost:8080/containers | python -mjson.tool
   curl -s -X GET -H 'Accept: application/json'
   http://localhost:8080/containers?state=running | python -mjson.tool
 
